33a1f1b32eb64fe1c2672a67876f0022c778dca2
unknown <dragos.susman@CJ002442PC.esp.local>
2014-11-12 14:19:16 +0200

    added more tests

--- WebTests/HomePageForMeTests/BasePageElement.py
+++ WebTests/HomePageForMeTests/BasePageElement.py
@@ -5,12 +5,13 @@ Created on Nov 11, 2014
 '''
 import unittest
 import this
+from lib2to3.tests.support import driver
 
 
 class BasePageElement(unittest.TestCase):
     
     # method used to find elements in page based on identifiers
-    def findElements(self, param, locator):
+    def findElement(self, param, locator):
         if param == 'class_name':
             return self.driver.find_element_by_class_name(locator)
         if param == 'id':
@@ -22,10 +23,20 @@ class BasePageElement(unittest.TestCase):
     
     # method used to get the text from all web elements in page
     def getTextFromElements(self, element):
-        
         # checks an element's text
-        titles = { self.findElements('xpath', '//div[@class="topBar"]/div/div/div/ul/li[1]/a') : 'For Me' }
-        self.assertEqual(element.text, titles[element], element + 'has the correct text')    
+        titles = { self.findElement('xpath', '//div[@class="topBar"]/div/div/div/ul/li[1]/a') : 'ForMe' }
+        self.assertEqual(element.text, titles[element], 'The' + element + 'has incorrect text')
+        
+    # open main page URL
+    def openHomePage(self):
+        URL = 'http://www.mobilinkgsm.com'
+        self.driver.get(URL)
+        
+    def getAttributeFromElements(self, attribute, element, string):
+        if attribute == 'href':  
+            self.assertEqual(element.get_attribute(attribute), string, 'The element tested does not redirect to correct link') 
+        elif attribute == 'class':
+            self.assertEqual(element.get_attribute(attribute), string, 'The element tested does not have the expected class attribute') 
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
--- WebTests/HomePageForMeTests/HomePage.py
+++ WebTests/HomePageForMeTests/HomePage.py
@@ -8,71 +8,119 @@ from selenium import webdriver
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from lib2to3.tests.support import driver
 from BasePageElement import BasePageElement
+import time
+
+URL = 'http://www.mobilink.com.pk/'
 
 class HomePageUITest(BasePageElement):
     
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(1)
+        self.driver.implicitly_wait(10)
         self.driver.maximize_window()
 
     def tearDown(self):
-        self.driver.quit()
+        self.driver.close()
 
     def testTopBarElements(self):
-        URL = 'http://www.mobilinkgsm.com'
-        self.driver.get(URL)
-        self.assertTrue(self.findElements('class_name', 'topBar'), 'TopBar is not displayed')
+        self.openHomePage()
+        self.assertTrue(self.findElement('class_name', 'topBar'), 'TopBar is not displayed')
         # For Me
-        self.assertEqual(self.findElements('xpath', '//div[@class="topBar"]/div/div/div/ul/li[1]/a').get_attribute('href'), 'http://www.mobilink.com.pk/', 
-                         'For Me option in topBar Menu does not have the correct link or is not displayed in home page')
-        self.assertEqual(self.findElements('xpath', '//div[@class="topBar"]/div/div/div/ul/li[1]/a').text, 'For Me', 
-                         'For Me option text is incorrect')
-        
+        forMeOption = self.findElement('xpath', '//div[@class="topBar"]/div/div/div/ul/li[1]/a')
+        forBusinessOption = self.findElement('xpath', '//div[@class="topBar"]/div/div/div/ul/li[2]/a')
+        homeLinkInTopBar = self.findElement('xpath', '//*[@id="menu-item-2943"]/a')
+        crLinkInTopBar =  self.findElement('xpath', '//*[@id="menu-item-4737"]/a')
+        careersLinkInTopBar = self.findElement('xpath', '//*[@id="menu-item-3549"]/a')
+        aboutUsLinkInTopBar = self.findElement('xpath', '//*[@id="menu-item-804"]/a')
+        ecareLinkInTopBar = self.findElement('xpath', '//*[@id="menu-item-4390"]/a')
+ 
+        self.getAttributeFromElements('href', forMeOption, URL)
+        self.assertEqual(forMeOption.text, 'For Me', 'For Me option text is incorrect')
+         
         # For Business
-        self.assertEqual(self.findElements('xpath', '//div[@class="topBar"]/div/div/div/ul/li[2]/a').get_attribute('href'), 'http://www.mobilink.com.pk/business', 
-                         'For Business option in topBar Menu does not have the correct link or is not displayed in home page')
-        self.assertEqual(self.findElements('xpath', '//div[@class="topBar"]/div/div/div/ul/li[2]/a').text, 'For Business', 
-                         'For Business option text is incorrect')
-        
+        self.getAttributeFromElements('href', forBusinessOption, URL + 'business')
+        self.assertEqual(forBusinessOption.text, 'For Business', 'For Business option text is incorrect')
+         
         # links in topBar
-        self.assertTrue(self.findElements('id', 'menu-top-navigation').is_displayed(), 'Links in topBar are not displayed')
-        
+        self.assertTrue(self.findElement('id', 'menu-top-navigation').is_displayed(), 'Links in topBar are not displayed')
+         
         # home link
-        self.assertTrue(self.findElements('id', 'menu-item-2943').is_displayed(), 'Home link in topBar is not displayed')
-        #FIXME: self.assertEqual(self.findElements('xpath', '//*[@id="menu-item-2943"]/a').get_attribute('href'), 'http://www.mobilink.com.pk/', 
-        #                 'Home link in topBar Menu does not redirect to correct page')
-        self.assertEqual(self.findElements('xpath', '//*[@id="menu-item-2943"]/a').text, 'Home',
+        self.assertTrue(self.findElement('id', 'menu-item-2943').is_displayed(), 'Home link in topBar is not displayed')
+        self.getAttributeFromElements('href', homeLinkInTopBar, URL)
+        self.assertEqual(homeLinkInTopBar.text, 'Home',
                          'Home link does not have the correct text')
-        
+         
         # CR - corporate responsibilty
-        self.assertTrue(self.findElements('id', 'menu-item-4737').is_displayed(), 'CR link in topBar is not displayed')
-        self.assertEqual(self.findElements('xpath', '//*[@id="menu-item-4737"]/a').get_attribute('href'), 'http://www.mobilink.com.pk/about-us/corporate-responsibility/', 
-                         'CR link in topBar Menu does not redirect to correct page')
-        self.assertEqual(self.findElements('xpath', '//*[@id="menu-item-4737"]/a').text, 'CR',
+        self.assertTrue(self.findElement('id', 'menu-item-4737').is_displayed(), 'CR link in topBar is not displayed')
+        self.getAttributeFromElements('href', crLinkInTopBar, URL + 'about-us/corporate-responsibility/')
+        self.assertEqual(crLinkInTopBar.text, 'CR',
                          'CR link does not have the correct text')
-        
+         
         # Careers
-        self.assertTrue(self.findElements('id', 'menu-item-3549').is_displayed(), 'Careers link in topBar is not displayed')
-        self.assertEqual(self.findElements('xpath', '//*[@id="menu-item-3549"]/a').get_attribute('href'), 'http://jobs.mobilinkgsm.com/', 
-                         'Careers link in topBar Menu does not redirect to correct page')
-        self.assertEqual(self.findElements('xpath', '//*[@id="menu-item-3549"]/a').text, 'Careers',
+        self.assertTrue(self.findElement('id', 'menu-item-3549').is_displayed(), 'Careers link in topBar is not displayed')
+        self.getAttributeFromElements('href', careersLinkInTopBar, 'http://jobs.mobilinkgsm.com/')
+        self.assertEqual(careersLinkInTopBar.text, 'Careers',
                          'Careers link does not have the correct text')
-        
+         
         # About Us
-        self.assertTrue(self.findElements('id', 'menu-item-804').is_displayed(), 'About us link in topBar is not displayed')
-        self.assertEqual(self.findElements('xpath', '//*[@id="menu-item-804"]/a').get_attribute('href'), 'http://www.mobilink.com.pk/about-us/', 
-                         'About us link in topBar Menu does not redirect to correct page')
-        self.assertEqual(self.findElements('xpath', '//*[@id="menu-item-804"]/a').text, 'About Us',
+        self.assertTrue(self.findElement('id', 'menu-item-804').is_displayed(), 'About us link in topBar is not displayed')
+        self.getAttributeFromElements('href', aboutUsLinkInTopBar, URL + 'about-us/')
+        self.assertEqual(aboutUsLinkInTopBar.text, 'About Us',
                          'About Us link does not have the correct text')
-        
+         
         # eCare
-        self.assertTrue(self.findElements('id', 'menu-item-4390').is_displayed(), 'eCare link in topBar is not displayed')
-        self.assertEqual(self.findElements('xpath', '//*[@id="menu-item-4390"]/a').get_attribute('href'), 'https://ecare.mobilinkgsm.com/', 
-                         'eCare link in topBar Menu does not redirect to correct page')
-        self.assertEqual(self.findElements('xpath', '//*[@id="menu-item-4390"]/a').text, 'eCare',
+        self.assertTrue(self.findElement('id', 'menu-item-4390').is_displayed(), 'eCare link in topBar is not displayed')
+        self.getAttributeFromElements('href', ecareLinkInTopBar, 'https://ecare.mobilinkgsm.com/')
+        self.assertEqual(ecareLinkInTopBar.text, 'eCare',
                          'eCare link does not have the correct text')
-        
+         
+    def testLogo(self):
+        self.openHomePage()
+        self.getAttributeFromElements('class', self.findElement('xpath', '//div[@class="mobilink_website"]/div[2]/div[1]/div/div[1]/h2'), 'logo')
+        self.assertEqual(self.findElement('xpath', '//div[@class="mobilink_website"]/div[2]/div[1]/div/div[1]/h2/a').text, 'Mobilink', 
+                         'The text in logo on homepage is not correct')
+         
+    def testSocialLinks(self):
+        self.openHomePage()
+        # Facebook
+        self.getAttributeFromElements('href', self.findElement('xpath', '//div[@class="mobilink_website"]/div[2]/div[1]/div/div[2]/ul/li[2]/a'), 'http://www.facebook.com/Mobilink')
+        self.getAttributeFromElements('class', self.findElement('xpath', '//div[@class="mobilink_website"]/div[2]/div[1]/div/div[2]/ul/li[2]/a'), 'icon_facebook')
+        # Linkedin
+        self.getAttributeFromElements('href', self.findElement('xpath', '//div[@class="mobilink_website"]/div[2]/div[1]/div/div[2]/ul/li[3]/a'), 'https://twitter.com/Mobilink')
+        self.getAttributeFromElements('class', self.findElement('xpath', '//div[@class="mobilink_website"]/div[2]/div[1]/div/div[2]/ul/li[3]/a'), 'icon_twitter')
+         
+    def testMainMenuElements(self):
+        self.openHomePage()
+        self.assertTrue(self.findElement('id', 'megaMenu').is_displayed(), 'Main menu is not displayed on home page')
+        # Prepaid option
+        self.assertTrue(self.findElement('xpath', '//*[@id="menu-item-82"]/a').get_attribute('href').find('prepaid'), 
+                        'Prepaid option in main menu does not redirect to correct link')
+        self.assertEqual(self.findElement('xpath', '//*[@id="menu-item-82"]/a/span').text, 'Prepaid',
+                        'Prepaid option in main menu has incorrect text')
+        # Postpaid option
+        self.assertTrue(self.findElement('xpath', '//*[@id="menu-item-178"]/a').get_attribute('href').find('postpaid'), 
+                        'Postpaid option in main menu does not redirect to correct link')
+        self.assertEqual(self.findElement('xpath', '//*[@id="menu-item-178"]/a/span').text, 'Postpaid',
+                        'Postpaid option in main menu has incorrect text')
+        # Mobile Internet
+        self.assertTrue(self.findElement('xpath', '//*[@id="menu-item-4334"]/a').get_attribute('href').find('mobile-internet'), 
+                        'Mobile Internet option in main menu does not redirect to correct link')
+        self.assertEqual(self.findElement('xpath', '//*[@id="menu-item-4334"]/a/span').text, 'Mobile Internet',
+                        'Mobile Internet option in main menu has incorrect text')
+        # Intl Roaming
+        self.assertTrue(self.findElement('xpath', '//*[@id="menu-item-9010"]/a').get_attribute('href').find('ir'), 
+                        'Intl Roaming option in main menu does not redirect to correct link')
+        # Value Added Services
+        self.assertTrue(self.findElement('xpath', '//*[@id="menu-item-408"]/a').get_attribute('href').find('vas'), 
+                        'Value Added Services option in main menu does not redirect to correct link')
+        self.assertEqual(self.findElement('xpath', '//*[@id="menu-item-408"]/a/span').text, 'Value Added Services',
+                        'Value Added Services option in main menu has incorrect text')
+        # Customer Care
+        self.assertTrue(self.findElement('xpath', '//*[@id="menu-item-185"]/a').get_attribute('href').find('customer-care'), 
+                        'Customer Care option in main menu does not redirect to correct link')
+        self.assertEqual(self.findElement('xpath', '//*[@id="menu-item-185"]/a/span').text, 'Customer Care',
+                        'Customer Care option in main menu has incorrect text')
+    
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
