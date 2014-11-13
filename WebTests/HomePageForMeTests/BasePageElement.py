a820131b478c263f04b1a194acc4f27e3875583c
unknown <dragos.susman@CJ002442PC.esp.local>
2014-11-13 14:51:21 +0200

    modified setup and teardown; added some more tests

--- WebTests/HomePageForMeTests/BasePageElement.py
+++ WebTests/HomePageForMeTests/BasePageElement.py
@@ -4,22 +4,23 @@ Created on Nov 11, 2014
 @author: dragos.susman
 '''
 import unittest
-import this
-from lib2to3.tests.support import driver
-
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium import webdriver
+import os, json
 
 class BasePageElement(unittest.TestCase):
     
     # method used to find elements in page based on identifiers
     def findElement(self, param, locator):
         if param == 'class_name':
-            return self.driver.find_element_by_class_name(locator)
+            return self.Driver.find_element_by_class_name(locator)
         if param == 'id':
-            return self.driver.find_element_by_id(locator)
+            return self.Driver.find_element_by_id(locator)
         if param == 'name':
-            return self.driver.find_element_by_name(locator)
+            return self.Driver.find_element_by_name(locator)
         if param == 'xpath':
-            return self.driver.find_element_by_xpath(locator)
+            return self.Driver.find_element_by_xpath(locator)
     
     # method used to get the text from all web elements in page
     def getTextFromElements(self, element):
@@ -29,14 +30,53 @@ class BasePageElement(unittest.TestCase):
         
     # open main page URL
     def openHomePage(self):
-        URL = 'http://www.mobilinkgsm.com'
-        self.driver.get(URL)
+        #URL = 'http://www.mobilinkgsm.com'
+        self.Driver.get(self.Config['URL'])
         
     def getAttributeFromElements(self, attribute, element, string):
         if attribute == 'href':  
             self.assertEqual(element.get_attribute(attribute), string, 'The element tested does not redirect to correct link') 
         elif attribute == 'class':
-            self.assertEqual(element.get_attribute(attribute), string, 'The element tested does not have the expected class attribute') 
+            self.assertEqual(element.get_attribute(attribute), string, 'The element tested does not have the expected class attribute')
+        elif attribute == 'style':
+            self.assertEqual(element.get_attribute(attribute), string, 'The element tested does not have the expected style attribute')
+    def openSubMenuDropDown(self, element):
+        browser = self.Config['Browser']
+        # move to element function doesn't work well; the only solution was this rudimentary one
+        if browser == 'Chrome':
+            ActionChains(self.Driver).move_to_element(element).perform()
+        elif browser == 'Firefox':
+            ActionChains(self.Driver).move_to_element(element).perform()
+            ActionChains(self.Driver).move_to_element(element).perform()
+        # unless we set a sleep time the test fails
+        time.sleep(5)
+    
+    @staticmethod
+    def Configure(self):
+        jsonPath = os.getenv("CONFIG_JSON");
+        print ("CONFIG_JSON:" + jsonPath)
+        if (os.path.isfile(jsonPath)):
+            config = json.loads(open(jsonPath, 'r').read())
+        else:
+            raise NameError("Cannot find config json as a system variable")
+        currentDir = os.path.dirname(os.path.realpath(__file__))
+
+        self.Config = config
+        browser = self.Config['Browser']
+        args = None
+        if browser == 'Chrome':
+            chromedriver = self.Config['FilesDir'] + "\\chromedriver"
+            os.environ["webdriver.chrome.driver"] = str(chromedriver)
+            args = [chromedriver]
+        
+        driver = getattr(webdriver, browser)
+        if args is None:
+            self.Driver = driver()
+        else:
+            self.Driver = driver(*args)
+        self.browser = browser
+        self.Driver.maximize_window()
+        self.windows = [ self.Driver ]
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
--- WebTests/HomePageForMeTests/HomePage.py
+++ WebTests/HomePageForMeTests/HomePage.py
@@ -8,19 +8,16 @@ from selenium import webdriver
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from lib2to3.tests.support import driver
 from BasePageElement import BasePageElement
-import time
-
-URL = 'http://www.mobilink.com.pk/'
 
 class HomePageUITest(BasePageElement):
     
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(10)
-        self.driver.maximize_window()
+    @classmethod
+    def setUpClass(cls):
+        cls.Configure(cls)
 
-    def tearDown(self):
-        self.driver.close()
+    @classmethod
+    def tearDownClass(cls):
+        cls.Driver.quit()
 
     def testTopBarElements(self):
         self.openHomePage()
@@ -33,53 +30,53 @@ class HomePageUITest(BasePageElement):
         careersLinkInTopBar = self.findElement('xpath', '//*[@id="menu-item-3549"]/a')
         aboutUsLinkInTopBar = self.findElement('xpath', '//*[@id="menu-item-804"]/a')
         ecareLinkInTopBar = self.findElement('xpath', '//*[@id="menu-item-4390"]/a')
- 
-        self.getAttributeFromElements('href', forMeOption, URL)
+  
+        self.getAttributeFromElements('href', forMeOption, self.Config['PKURL'])
         self.assertEqual(forMeOption.text, 'For Me', 'For Me option text is incorrect')
-         
+          
         # For Business
-        self.getAttributeFromElements('href', forBusinessOption, URL + 'business')
+        self.getAttributeFromElements('href', forBusinessOption, self.Config['PKURL'] + 'business')
         self.assertEqual(forBusinessOption.text, 'For Business', 'For Business option text is incorrect')
-         
+          
         # links in topBar
         self.assertTrue(self.findElement('id', 'menu-top-navigation').is_displayed(), 'Links in topBar are not displayed')
-         
+          
         # home link
         self.assertTrue(self.findElement('id', 'menu-item-2943').is_displayed(), 'Home link in topBar is not displayed')
-        self.getAttributeFromElements('href', homeLinkInTopBar, URL)
+        self.getAttributeFromElements('href', homeLinkInTopBar, self.Config['PKURL'])
         self.assertEqual(homeLinkInTopBar.text, 'Home',
                          'Home link does not have the correct text')
-         
+          
         # CR - corporate responsibilty
         self.assertTrue(self.findElement('id', 'menu-item-4737').is_displayed(), 'CR link in topBar is not displayed')
-        self.getAttributeFromElements('href', crLinkInTopBar, URL + 'about-us/corporate-responsibility/')
+        self.getAttributeFromElements('href', crLinkInTopBar, self.Config['PKURL'] + 'about-us/corporate-responsibility/')
         self.assertEqual(crLinkInTopBar.text, 'CR',
                          'CR link does not have the correct text')
-         
+          
         # Careers
         self.assertTrue(self.findElement('id', 'menu-item-3549').is_displayed(), 'Careers link in topBar is not displayed')
         self.getAttributeFromElements('href', careersLinkInTopBar, 'http://jobs.mobilinkgsm.com/')
         self.assertEqual(careersLinkInTopBar.text, 'Careers',
                          'Careers link does not have the correct text')
-         
+          
         # About Us
         self.assertTrue(self.findElement('id', 'menu-item-804').is_displayed(), 'About us link in topBar is not displayed')
-        self.getAttributeFromElements('href', aboutUsLinkInTopBar, URL + 'about-us/')
+        self.getAttributeFromElements('href', aboutUsLinkInTopBar, self.Config['PKURL'] + 'about-us/')
         self.assertEqual(aboutUsLinkInTopBar.text, 'About Us',
                          'About Us link does not have the correct text')
-         
+          
         # eCare
         self.assertTrue(self.findElement('id', 'menu-item-4390').is_displayed(), 'eCare link in topBar is not displayed')
         self.getAttributeFromElements('href', ecareLinkInTopBar, 'https://ecare.mobilinkgsm.com/')
         self.assertEqual(ecareLinkInTopBar.text, 'eCare',
                          'eCare link does not have the correct text')
-         
+          
     def testLogo(self):
         self.openHomePage()
         self.getAttributeFromElements('class', self.findElement('xpath', '//div[@class="mobilink_website"]/div[2]/div[1]/div/div[1]/h2'), 'logo')
         self.assertEqual(self.findElement('xpath', '//div[@class="mobilink_website"]/div[2]/div[1]/div/div[1]/h2/a').text, 'Mobilink', 
                          'The text in logo on homepage is not correct')
-         
+          
     def testSocialLinks(self):
         self.openHomePage()
         # Facebook
@@ -88,7 +85,7 @@ class HomePageUITest(BasePageElement):
         # Linkedin
         self.getAttributeFromElements('href', self.findElement('xpath', '//div[@class="mobilink_website"]/div[2]/div[1]/div/div[2]/ul/li[3]/a'), 'https://twitter.com/Mobilink')
         self.getAttributeFromElements('class', self.findElement('xpath', '//div[@class="mobilink_website"]/div[2]/div[1]/div/div[2]/ul/li[3]/a'), 'icon_twitter')
-         
+          
     def testMainMenuElements(self):
         self.openHomePage()
         self.assertTrue(self.findElement('id', 'megaMenu').is_displayed(), 'Main menu is not displayed on home page')
@@ -120,7 +117,43 @@ class HomePageUITest(BasePageElement):
                         'Customer Care option in main menu does not redirect to correct link')
         self.assertEqual(self.findElement('xpath', '//*[@id="menu-item-185"]/a/span').text, 'Customer Care',
                         'Customer Care option in main menu has incorrect text')
+        
+    def testPrepaidSubMenuDropDown(self):
+        self.openHomePage()
+        prepaidSubMenuDropDown = self.findElement('xpath', '//*[@id="menu-item-82"]/ul')
+        prepaidSubMenuLink = self.findElement('xpath', '//*[@id="menu-item-82"]/a/span')
+        self.getAttributeFromElements('style', prepaidSubMenuDropDown, 'display: none;')
+        self.openSubMenuDropDown(prepaidSubMenuLink)
+        browser = self.Config['Browser']
+        if browser == 'Firefox':
+            self.skipTest('Move to element method in webdriver has issues on Firefox')
+        elif browser == 'Chrome':
+            self.getAttributeFromElements('style', prepaidSubMenuDropDown, 'display: block;')
+            self.assertTrue(self.findElement('id', 'menu-item-56').is_displayed(), 'Packages option in submenu is not displayed')
+            self.assertTrue(self.findElement('id', 'menu-item-86').is_displayed(), 'Offers option in submenu is not displayed')
+            self.assertTrue(self.findElement('id', 'menu-item-3690').is_displayed(), 'Mobile Internet option in submenu is not displayed')
+            self.assertTrue(self.findElement('id', 'menu-item-407').is_displayed(), 'Value Added Services option in submenu is not displayed')
+            self.assertTrue(self.findElement('id', 'menu-item-83').is_displayed(), 'Prepaid intl roaming option in submenu is not displayed')
+            self.assertTrue(self.findElement('id', 'menu-item-143').is_displayed(), 'More option in submenu is not displayed')
     
+    def testPostpaidSubMenuDropDown(self):
+        self.openHomePage()
+        postpaidSubMenuLink = self.findElement('xpath', '//*[@id="menu-item-178"]/a/span')
+        postpaidSubMenuDropDown = self.findElement('xpath', '//*[@id="menu-item-178"]/ul')
+        self.getAttributeFromElements('style', postpaidSubMenuDropDown, 'display: none;')
+        self.openSubMenuDropDown(postpaidSubMenuLink)
+        browser = self.Config['Browser']
+        if browser == 'Firefox':
+            self.skipTest('Move to element method in webdriver has issues on Firefox')
+        elif browser == 'Chrome':
+            self.getAttributeFromElements('style', postpaidSubMenuDropDown, 'display: block;')
+            self.assertTrue(self.findElement('id', 'menu-item-546').is_displayed(), 'Packages option in submenu is not displayed')
+            self.assertTrue(self.findElement('id', 'menu-item-545').is_displayed(), 'Offers option in submenu is not displayed')
+            self.assertTrue(self.findElement('id', 'menu-item-3694').is_displayed(), 'Mobile Internet option in submenu is not displayed')
+            self.assertTrue(self.findElement('id', 'menu-item-547').is_displayed(), 'Value Added Services option in submenu is not displayed')
+            self.assertTrue(self.findElement('id', 'menu-item-551').is_displayed(), 'Prepaid intl roaming option in submenu is not displayed')
+            self.assertTrue(self.findElement('id', 'menu-item-1541').is_displayed(), 'More option in submenu is not displayed')
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
